4cb7185f04065362880a112e0da15edf457c8201
Yang Cao <yacao@microsoft.com>
2018-01-17 15:13:12 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,9 +34,6 @@ module.exports = function(grunt) {
         dest: 'dist/ba-<%= pkg.name %>.min.js'
       },
     },
-    qunit: {
-      files: ['test/**/*.html']
-    },
     jshint: {
       gruntfile: {
         options: {
@@ -77,11 +74,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify']);
 
 };
